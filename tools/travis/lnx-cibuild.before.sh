55a1d0542680b0ea8628c7bea167637761aae1fa
Hannes Roest <hroest@stanford.edu>
2017-07-26 11:54:31 -0700

    test travis cache 2

--- tools/travis/lnx-cibuild.before.sh
+++ tools/travis/lnx-cibuild.before.sh
@@ -15,24 +15,25 @@ function build_contrib {
   fi
 }
 
-# fetch contrib and build seqan
-git clone git://github.com/OpenMS/contrib/
-pushd contrib
-
-# we build seqan as the versions shipped in Ubuntu are not recent enough
-build_contrib SEQAN
-
-# we build WildMagic
-build_contrib WILDMAGIC
-
-# we build Eigen as the versions shipped in Ubuntu are not recent enough
-build_contrib EIGEN
-
-# we build Sqlite as the versions shipped in Ubuntu are not recent enough
-build_contrib SQLITE
-
-# leave contrib
-popd
+####  Now in cache? 
+#### # fetch contrib and build seqan
+#### git clone git://github.com/OpenMS/contrib/
+#### pushd contrib
+#### 
+#### # we build seqan as the versions shipped in Ubuntu are not recent enough
+#### build_contrib SEQAN
+#### 
+#### # we build WildMagic
+#### build_contrib WILDMAGIC
+#### 
+#### # we build Eigen as the versions shipped in Ubuntu are not recent enough
+#### build_contrib EIGEN
+#### 
+#### # we build Sqlite as the versions shipped in Ubuntu are not recent enough
+#### build_contrib SQLITE
+#### 
+#### # leave contrib
+#### popd
 
 # build custom cppcheck if we want to perform style tests
 if [ "${ENABLE_STYLE_TESTING}" = "ON" ]; then
