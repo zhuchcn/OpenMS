a6bc523934caeedb52a203569062bdd1bebf6f54
Hannes Roest <hroest@stanford.edu>
2017-12-27 22:59:02 -0500

    build

--- tools/travis/lnx-cibuild.before.sh
+++ tools/travis/lnx-cibuild.before.sh
@@ -56,7 +56,6 @@ build_contrib SQLITE
 # leave contrib
 popd
 
-
 # build custom cppcheck if we want to perform style tests
 if [ "${ENABLE_STYLE_TESTING}" = "ON" ]; then
   git clone git://github.com/danmar/cppcheck.git
@@ -66,8 +65,8 @@ if [ "${ENABLE_STYLE_TESTING}" = "ON" ]; then
   popd
 else
   # regular builds .. get the search engine executables via githubs SVN interface (as git doesn't allow single folder checkouts)
+  rm -rf _thirdparty
   svn export --force https://github.com/OpenMS/THIRDPARTY/trunk/Linux/64bit/ _thirdparty
   svn export --force https://github.com/OpenMS/THIRDPARTY/trunk/All/ _thirdparty
 fi
-
 
